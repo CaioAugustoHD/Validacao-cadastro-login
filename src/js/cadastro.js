3ae047be16333f74e16e791859327806d1a9a1cc
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-09-12 20:36:16 -0300

    adicionado script cadastro.js

---
+++ src/js/cadastro.js
@@ -0,0 +1,11 @@
+let cadUsuario = document.getElementById('cadUsuario');
+let cadCPF = document.getElementById('cadCPF');
+let cadSenha = document.getElementById('cadSenha');
+let botao = document.querySelector('button');
+
+botao.addEventListener('click', cadastrar);
+
+let listaUsuarios = [];
+
+function cadastrar(){
+}
