8d54ff704596cce54b50ed0c226ab3b4820d42e9
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-09-23 08:46:35 -0300

    Função de validação dos dados que serão cadastrados

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -3,7 +3,7 @@ let cadCPF = document.getElementById('cadCPF');
 let cadSenha = document.getElementById('cadSenha');
 let botaoCad = document.getElementById('botaoCad');
 
-botaoCad.addEventListener('click', cadastrar);
+botaoCad.addEventListener('click', validacao);
 
 let listaUsuarios = [];
 function lerListaUsuarios(){
@@ -21,8 +21,49 @@ class novoUsuario {
 function cadastrar(){
     let usuarioCadastrado = new novoUsuario (cadUsuario.value, cadCPF.value, cadSenha.value);
 
-    lerListaUsuarios();
     listaUsuarios.push(usuarioCadastrado);
 
     localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
+}
+
+function validacao(){
+
+    lerListaUsuarios();
+
+
+    let usuarioValido = true;
+    if(cadUsuario.value.length < 3){
+        alert('O nome de usuário deve possuir no mínimo 3 caracteres!')
+        usuarioValido = false;
+    } else {
+        listaUsuarios.forEach((usuario) => {
+            if(cadUsuario.value == usuario.usuario){
+                alert ('Esse nome de usuário já existe!');
+                usuarioValido = false;
+            }    
+        })
+    }
+
+    let cpfValido = true;
+    if(cadCPF.value.length != 11){
+        alert('CPF inválido!\nO CPF deve possuir 11 dígitos!');
+        cpfValido = false;
+    } else {
+        listaUsuarios.forEach((usuario) => {
+            if(cadCPF.value == usuario.cpf){
+                alert('Esse CPF já está cadastrado!');
+                cpfValido = false;
+            }
+        })
+    }
+
+    let senhaValida = true;
+    if(cadSenha.value.length < 5){
+        alert('Sua senha deve possuir no mínimo 5 caracteres!');
+        senhaValida = false;
+    }
+
+    if((usuarioValido) && (cpfValido) && (senhaValida)){
+        cadastrar();
+    }
 }
